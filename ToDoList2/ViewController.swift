c2befe7f03ad51b16dadac74b97418716b3eecb0
Visitor <visitor@Admins-MacBook-Pro-2.local>
2019-07-30 18:06:57 +0300

    Initial Commit

---
+++ ToDoList2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToDoList2
+//
+//  Created by Visitor on 30/07/2019.
+//  Copyright © 2019 Visitor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
